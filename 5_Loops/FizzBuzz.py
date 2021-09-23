c6456cc006bfb9d92f1f3affcc63d49f410b18af
fowzy <fwzsas@gmai.com>
2021-09-23 00:36:19 -0500

    UPDATE

--- 5_Loops/FizzBuzz.py
+++ 5_Loops/FizzBuzz.py
@@ -1,6 +1,11 @@
 
-number = input()
-
-if number % 3 
-for i in range(1,100+1):
-    print(i)
+# Fizz Buzz game
+for i in range(1, 100+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
